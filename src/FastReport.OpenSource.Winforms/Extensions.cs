c657aa08e089515157e2929d45276b0af536d395
Debanjan Chakraborty <frontlook.inc@gmail.com>
2020-12-16 02:57:11 +0530

    Print Setting Updated

--- src/FastReport.OpenSource.Winforms/Extensions.cs
+++ src/FastReport.OpenSource.Winforms/Extensions.cs
@@ -32,9 +32,31 @@ namespace FastReport.OpenSource.Winforms
             }
         }
 
+        public static void PrintWithDialogMod(this Report report)
+        {
+            using (var dlg = new PrintDialog())
+            {
+                dlg.AllowSomePages = true;
+                dlg.AllowSelection = true;
+                dlg.UseEXDialog = true;
+
+                if (dlg.ShowDialog() != DialogResult.OK) return;
+
+                report.PrintMod(dlg.PrinterSettings);
+            }
+        }
+
         public static void Print(this Report report, PrinterSettings settings = null)
         {
             var doc = report.PrepareDoc(settings);
+            if (doc == null) return;
+
+            doc.Print();
+            doc.Dispose();
+        }
+        public static void PrintMod(this Report report, PrinterSettings settings = null)
+        {
+            var doc = report.PrepareDocMod(settings);
             if (doc == null) return;
 
             doc.Print();
@@ -48,10 +70,26 @@ namespace FastReport.OpenSource.Winforms
 
             preview.Document = doc;
         }
+        public static void PreviewMod(this Report report, FRPrintPreviewControl preview, PrinterSettings settings = null)
+        {
+            var doc = report.PrepareDocMod(settings);
+            if (doc == null) return;
+
+            preview.Document = doc;
+        }
 
         public static void Preview(this Report report, PrinterSettings settings = null)
         {
             var doc = report.PrepareDoc(settings);
+            if (doc == null) return;
+
+            using (var preview = new FRPrintPreviewDialog() { Document = doc })
+                preview.ShowDialog();
+            doc.Dispose();
+        }
+        public static void PreviewMod(this Report report, PrinterSettings settings = null)
+        {
+            var doc = report.PrepareDocMod(settings);
             if (doc == null) return;
 
             using (var preview = new FRPrintPreviewDialog() { Document = doc })
@@ -86,6 +124,73 @@ namespace FastReport.OpenSource.Winforms
 
                 args.PageSettings.PaperSize = new PaperSize("Custom", (int)(ExportUtils.GetPageWidth(rPage) * scaleFactor * Units.HundrethsOfInch),
                                                                       (int)(ExportUtils.GetPageHeight(rPage) * scaleFactor * Units.HundrethsOfInch));
+            };
+
+            doc.PrintPage += (sender, args) =>
+            {
+                using (var ms = new MemoryStream())
+                {
+                    exp.PageRange = PageRange.PageNumbers;
+                    exp.PageNumbers = $"{page + 1}";
+                    exp.Export(report, ms);
+
+                    args.Graphics.DrawImage(Image.FromStream(ms), args.PageBounds);
+                }
+
+                page++;
+
+                args.HasMorePages = page < report.PreparedPages.Count;
+            };
+
+            doc.EndPrint += (sender, args) => page = 0;
+            doc.Disposed += (sender, args) => exp?.Dispose();
+
+            return doc;
+        }
+
+
+
+        private static PrintDocument PrepareDocMod(this Report report, PrinterSettings settings = null)
+        {
+            if (report.PreparedPages.Count < 1)
+            {
+                report.Prepare();
+                if (report.PreparedPages.Count < 1) return null;
+            }
+
+            var page = 0;
+            var exp = new ImageExport { ImageFormat = ImageExportFormat.Png, Resolution = 600 };
+
+            var doc = new PrintDocument { DocumentName = report.Name };
+            if (settings != null)
+                doc.PrinterSettings = settings;
+
+            // Ajustando o tamanho da pagina
+            doc.QueryPageSettings += (sender, args) =>
+            {
+                if (settings != null)
+                {
+                    args.PageSettings = settings.DefaultPageSettings;
+
+                }
+                else
+                {
+                    var rPage = report.PreparedPages.GetPage(page);
+
+                    args.PageSettings.Landscape = rPage.Landscape;
+                    args.PageSettings.PaperSize = new PaperSize("Custom",
+                        (int)(ExportUtils.GetPageWidth(rPage) * scaleFactor * Units.HundrethsOfInch),
+                        (int)(ExportUtils.GetPageHeight(rPage) * scaleFactor * Units.HundrethsOfInch));
+                    args.PageSettings.Margins =
+                        new Margins(
+                        (int)(scaleFactor * rPage.LeftMargin * Units.HundrethsOfInch),
+                        (int)(scaleFactor * rPage.RightMargin * Units.HundrethsOfInch),
+                        (int)(scaleFactor * rPage.TopMargin * Units.HundrethsOfInch),
+                        (int)(scaleFactor * rPage.BottomMargin * Units.HundrethsOfInch));
+                }
+
+
+
             };
 
             doc.PrintPage += (sender, args) =>
